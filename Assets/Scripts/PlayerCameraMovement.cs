d490253b8fc72a277ec17eda38f89153921dc853
Braden Walters <vc@braden-walters.info>
2019-02-24 22:20:45 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerCameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerCameraMovement : MonoBehaviour {
+  public float speed = 2f;
+
+  private Rigidbody rb;
+
+  void Start() {
+    this.rb = this.GetComponent<Rigidbody>();
+  }
+
+  void FixedUpdate() {
+    var horizontal = Input.GetAxis("Horizontal") * this.speed;
+    var vertical = Input.GetAxis("Vertical") * this.speed;
+    if (horizontal != 0f || vertical != 0f) {
+      var movement = new Vector3(horizontal, vertical, 0f);
+      movement = Vector3.ClampMagnitude(movement, this.speed);
+
+      this.rb.velocity = movement;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+RocketThing
+===============
+
+A game built in Unity 3D that is inspired by Missile Game.
