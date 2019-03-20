9560a42f7000a1e0ee1f04bd47836c7cf3e1546b
Braden Walters <vc@braden-walters.info>
2019-03-20 22:00:18 +0100

    Disc generator

---
+++ Assets/Scripts/DiscGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class DiscGenerator : MonoBehaviour {
+  [SerializeField] private GameObject[] discPrefabs;
+  [SerializeField] private float beginEmitFrequency = 5f;
+  [SerializeField] private float endEmitFrequency = 1.5f;
+  [SerializeField] private float emitFrequncyChangeRate = 0.05f;
+
+  private float currentEmitFrequency;
+  private float timeSinceEmit = 0f;
+
+  void Start() {
+    this.currentEmitFrequency = this.beginEmitFrequency;
+  }
+
+  void Update() {
+    if (this.currentEmitFrequency > this.endEmitFrequency)
+      this.currentEmitFrequency -= this.emitFrequncyChangeRate * Time.deltaTime;
+    else
+      this.currentEmitFrequency = this.endEmitFrequency;
+
+    this.timeSinceEmit += Time.deltaTime;
+    if (this.timeSinceEmit > this.currentEmitFrequency) {
+      Emit();
+      this.timeSinceEmit = 0f;
+    }
+  }
+
+  private void Emit() {
+    var idx = new System.Random().Next() % this.discPrefabs.Length;
+    var discPrefab = this.discPrefabs[idx];
+
+    GameObject.Instantiate(
+      discPrefab,
+      this.transform.position,
+      this.transform.rotation);
+  }
+}
