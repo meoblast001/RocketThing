948eece49f827a65a95104f1d9cf63751f627773
Braden Walters <vc@braden-walters.info>
2019-03-25 20:27:22 +0100

    Random rotation and initial rotation of disc

--- Assets/Scripts/DiscMovement.cs
+++ Assets/Scripts/DiscMovement.cs
@@ -2,14 +2,19 @@
 
 [RequireComponent(typeof(Rigidbody))]
 public class DiscMovement : MonoBehaviour {
+  public float zMovement = -6f;
+
   private Rigidbody rb;
+  private float zAngularVelocity;
 
   void Start() {
     this.rb = this.GetComponent<Rigidbody>();
+    this.rb.rotation = Quaternion.Euler(0f, 0f, Random.value * 360f);
+    this.zAngularVelocity = (Random.value - 0.5f) * 2f;
   }
 
   void Update() {
-    this.rb.velocity = new Vector3(0f, 0f, -6f);
-    this.rb.angularVelocity = new Vector3(0f, 0f, -1f);
+    this.rb.velocity = new Vector3(0f, 0f, this.zMovement);
+    this.rb.angularVelocity = new Vector3(0f, 0f, this.zAngularVelocity);
   }
 }
