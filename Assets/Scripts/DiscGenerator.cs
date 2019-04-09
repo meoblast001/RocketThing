a530d2aea23384413eb3fcb7ea537bf9ea7a3ce7
Braden Walters <vc@braden-walters.info>
2019-04-09 16:41:09 +0200

    Increasing velocity of discs from generator

--- Assets/Scripts/DiscGenerator.cs
+++ Assets/Scripts/DiscGenerator.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 public class DiscGenerator : MonoBehaviour {
   [SerializeField] private GameObject[] discPrefabs;
   [SerializeField] private float beginEmitFrequency = 5f;
-  [SerializeField] private float endEmitFrequency = 1.5f;
+  [SerializeField] private float endEmitFrequency = 2f;
   [SerializeField] private float emitFrequncyChangeRate = 0.05f;
+  [SerializeField] private float beginDiscSpeed = -6f;
+  [SerializeField] private float endDiscSpeed = -10f;
+  [SerializeField] private float discSpeedChangeRate = 0.05f;
 
   private float currentEmitFrequency;
+  private float currentDiscSpeed;
   private float timeSinceEmit = 0f;
 
   void Start() {
     this.currentEmitFrequency = this.beginEmitFrequency;
+    this.currentDiscSpeed = this.beginDiscSpeed;
 
     EventSystem.Subscribe(
       GameManager.GameStateChangeEvent,
@@ -30,6 +35,11 @@ public class DiscGenerator : MonoBehaviour {
     else
       this.currentEmitFrequency = this.endEmitFrequency;
 
+    if (this.currentDiscSpeed > this.endDiscSpeed)
+      this.currentDiscSpeed -= this.discSpeedChangeRate * Time.deltaTime;
+    else
+      this.currentDiscSpeed = this.endDiscSpeed;
+
     this.timeSinceEmit += Time.deltaTime;
     if (this.timeSinceEmit > this.currentEmitFrequency) {
       Emit();
@@ -41,10 +51,16 @@ public class DiscGenerator : MonoBehaviour {
     var idx = new System.Random().Next() % this.discPrefabs.Length;
     var discPrefab = this.discPrefabs[idx];
 
-    GameObject.Instantiate(
+    var discInstanceObj = GameObject.Instantiate(
       discPrefab,
       this.transform.position,
       this.transform.rotation);
+    var discInstance = discInstanceObj.GetComponent<DiscMovement>();
+    if (discInstance != null)
+      discInstance.zMovement = this.currentDiscSpeed;
+    else
+      Debug.LogError("Disc instance created by generator does not have "
+        + nameof(DiscMovement));
   }
 
   private void OnGameStateChangeEvent(object sender, EventArgs e) {
